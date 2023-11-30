95388fd6c2ebc3891327f55d3f711a56c462409e
camiz <camiz5848@gmail.com>
2023-11-29 23:05:23 -0500

    fix graphs

--- YOLOv8_Models/Code/ultralytics_detect.py
+++ YOLOv8_Models/Code/ultralytics_detect.py
@@ -9,14 +9,12 @@ def plot_detection_comparison(training_dir):
 
     plt.subplot(1, 2, 1)
     img = cv2.imread(f'{training_dir}/val_batch0_labels.jpg')
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.title("Labels", fontsize=5)
     plt.axis('off')
     plt.imshow(img)
 
     plt.subplot(1, 2, 2)
     img = cv2.imread(f'{training_dir}/val_batch0_pred.jpg')
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.title("Detections", fontsize=5)
     plt.axis('off')
     plt.imshow(img)
